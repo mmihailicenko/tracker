3cd0425d78a5ef1df183b860fb17d629c62e6d02
mikhail.mihailicenko <mikhail.mihailicenko@gmail.com>
2019-07-25 07:28:24 +0300

    Chapter 22

--- app/routes/witnesses.js
+++ app/routes/witnesses.js
@@ -2,11 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    let witnessRecord = this.store.createRecord('witness', {
-      fName: 'Todd',
-      lName: 'Weasley',
-      mailAddress: 'todd.weasley@better.com'
-    });
-    return [witnessRecord];
+    return this.store.findAll('witness');
   }
 });
